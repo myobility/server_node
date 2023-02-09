5fac38f86e6064603d1bbd8f3513311187530a22
lee7198 <lee7198@naver.com>
2023-02-09 11:09:21 +0900

    setting frontend

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
 import express from "express";
+import path from "path";
 
+const __dirname = path.resolve();
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + "/src/public"));
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/*", (req, res) => {
+  res.redirect("/");
+});
 
-app.listen(3000);
+const portNum = 3000;
+const handleListen = () =>
+  console.log(`Listening no http://localhost:${portNum}}`);
+app.listen(portNum, handleListen);
