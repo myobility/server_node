40f41462df0b99f0546752063f6385bc5e260a33
lee7198 <lee7198@naver.com>
2023-02-21 03:10:07 +0900

    cors3

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import express from "express";
 import path from "path";
 
 const __dirname = path.resolve();
+const cors = require("cors");
 const app = express();
 const portNum = 3000;
 
@@ -13,7 +14,22 @@ const portNum = 3000;
 
 const waitingList = [];
 
-app.use(express.static(path.join(__dirname, "client", "dist")));
+// app.use(express.static(path.join(__dirname, "client", "dist")));
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 app.get("/", (req, res) => {
   // res.render("home");
   // res.sendFile(path.join(__dirname, "dist", "index.html"));
